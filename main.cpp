7b97591c110c7a4739ea165e48f3edcb037fa34f
Oleksandr Kolomiiets <alexcrimson0818@gmail.com>
2023-12-17 04:10:28 +0200

    Add timer

--- main.cpp
+++ main.cpp
@@ -2,9 +2,14 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <vector>
+#include <string>
 
 using namespace std;
 using namespace sf;
+
+static bool timerStarted;
+Clock gameClock;
 
 class Cell {
 private:
@@ -125,28 +130,14 @@ public:
         return count;
     }
 
-    bool revealCell(int x, int y) {
-        if (x >= 0 && x < rows && y >= 0 && y < columns) {
-            cells[x][y].reveal();
-            return true;
-        }
-        return false;
-    }
-
     void markCell(int x, int y) {
         if (x >= 0 && x < rows && y >= 0 && y < columns) {
             if (!cells[x][y].getMarked() && countMarkedCells() < mineCount) {
                 cells[x][y].mark();
             }
-            else if (cells[x][y].getMarked() && countMarkedCells() > 0){
+            else if (cells[x][y].getMarked() && countMarkedCells() > 0) {
                 cells[x][y].mark();
             }
-        }
-    }
-
-    void unmarkCell(int x, int y) {
-        if (x >= 0 && x < rows && y >= 0 && y < columns) {
-            cells[x][y].mark();
         }
     }
 
@@ -168,22 +159,25 @@ private:
     bool gameOver;
     bool revealMode;
 public:
-    Minesweeper(int rows, int columns) : rows(rows), columns(columns), mineGrid(rows, columns), normalGrid(rows, columns), gameOver(false), revealMode(true), win(false){}
+    Minesweeper(int rows, int columns) : rows(rows), columns(columns), mineGrid(rows, columns), normalGrid(rows, columns), gameOver(false), revealMode(true), win(false) {}
 
     bool win = false;
 
     void StartGame() {
         gameOver = false;
-
+        win = false;
+        timerStarted = false;
+        gameClock.restart();
         normalGrid = Grid(rows, columns);
     }
+
 
     void Click(int x, int y) {
         if (gameOver) {
             return;
         }
 
-        Cell& clickedCell = const_cast<Cell &>(normalGrid.getCells()[x][y]);
+        Cell& clickedCell = const_cast<Cell&>(normalGrid.getCells()[x][y]);
 
         if (!clickedCell.getRevealed() && !clickedCell.getMarked()) {
             clickedCell.reveal();
@@ -191,7 +185,8 @@ public:
             if (clickedCell.getIsMine()) {
                 clickedCell.explode();
                 Defeat();
-            } else {
+            }
+            else {
                 if (normalGrid.countAdjacentBombs(x, y) == 0) {
                     revealEmptyCells(x, y);
                 }
@@ -251,11 +246,13 @@ public:
     void Defeat() {
         normalGrid.revealAllBombs();
         gameOver = true;
+        timerStarted = false;
     }
 
     void Win() {
         gameOver = true;
         win = true;
+        timerStarted = false;
     }
 
     bool isGameOver() const {
@@ -297,11 +294,16 @@ private:
     Texture ModeTextureMark;
     Sprite ModeSprite;
 
+    Texture timerDigitTextures[11];
+    Sprite timerSprite;
+
+    float elapsedTime;
+
     void loadDigitTextures() {
         for (int i = 0; i <= 9; ++i) {
             digitTextures.push_back(Texture());
-            digitTextures.back().loadFromFile("/Users/zakerden1234/Desktop/Minesweeper/Sprites/Counts/" + to_string(i) + "s.png");
-            emptyDigitTexture.loadFromFile("/Users/zakerden1234/Desktop/Minesweeper/Sprites/Counts/s.png");
+            digitTextures.back().loadFromFile("Sprites/Counts/" + to_string(i) + "s.png");
+            emptyDigitTexture.loadFromFile("Sprites/Counts/s.png");
         }
     }
 
@@ -340,15 +342,16 @@ private:
         }
     }
 
-    void loadRevealModeTextures() {
-        ModeTextureReveal.loadFromFile("/Users/zakerden1234/Desktop/Minesweeper/Sprites/R.png");
-        ModeTextureMark.loadFromFile("/Users/zakerden1234/Desktop/Minesweeper/Sprites/M.png");
+void loadRevealModeTextures() {
+        ModeTextureReveal.loadFromFile("Sprites/R.png");
+        ModeTextureMark.loadFromFile("Sprites/M.png");
     }
 
     void drawRevealMode() {
         if (game.isRevealMode()) {
             ModeSprite.setTexture(ModeTextureReveal);
-        } else {
+        }
+        else {
             ModeSprite.setTexture(ModeTextureMark);
         }
 
@@ -358,28 +361,65 @@ private:
         window.draw(ModeSprite);
     }
 
+    void drawTimerDigits(float x, float y, float scale) {
+        string elapsedTimeString = to_string(static_cast<int>(elapsedTime));
+
+        while (elapsedTimeString.length() < 3) {
+            elapsedTimeString = "0" + elapsedTimeString;
+        }
+
+        float digitWidth = 13.0f * scale;
+
+        for (size_t i = 0; i < 3; ++i) {
+            char currentChar = elapsedTimeString[i];
+            Sprite digitSprite;
+
+            if (currentChar >= '0' && currentChar <= '9') {
+                int digit = currentChar - '0';
+                digitSprite.setTexture(timerDigitTextures[digit]);
+            }
+            else {
+                digitSprite.setTexture(timerDigitTextures[10]);
+            }
+
+            digitSprite.setPosition(x + i * digitWidth, y);
+            digitSprite.setScale(scale, scale);
+            window.draw(digitSprite);
+        }
+    }
+
+    void loadTimerDigitTextures() {
+        for (int i = 0; i <= 9; ++i) {
+            timerDigitTextures[i].loadFromFile("Sprites/Counts/" + to_string(i) + "s.png");
+        }
+        timerDigitTextures[10].loadFromFile("Sprites/Counts/s.png");
+    }
+
 
 public:
-    Render(int rows, int columns, Minesweeper& game) : game(game), window(VideoMode(columns * 60, rows * 72), "Minesweeper") {
-        emptyTexture.loadFromFile("/Users/zakerden1234/Desktop/Minesweeper/Sprites/0.png");
-        revealedTexture.loadFromFile("/Users/zakerden1234/Desktop/Minesweeper/Sprites/Check.png");
-        markedTexture.loadFromFile("/Users/zakerden1234/Desktop/Minesweeper/Sprites/Flag.png");
-        unmarkedTexture.loadFromFile("/Users/zakerden1234/Desktop/Minesweeper/Sprites/Cell.png");
-        bombTexture.loadFromFile("/Users/zakerden1234/Desktop/Minesweeper/Sprites/Bomb.png");
-        bombBoomTexture.loadFromFile("/Users/zakerden1234/Desktop/Minesweeper/Sprites/BombLose.png");
+
+    Render(int rows, int columns, Minesweeper& game) : game(game), window(VideoMode(columns * 60, rows * 72), "Minesweeper"), elapsedTime(0.0f) {
+        timerStarted = false;
+        emptyTexture.loadFromFile("Sprites/0.png");
+        revealedTexture.loadFromFile("Sprites/Check.png");
+        markedTexture.loadFromFile("Sprites/Flag.png");
+        unmarkedTexture.loadFromFile("Sprites/Cell.png");
+        bombTexture.loadFromFile("Sprites/Bomb.png");
+        bombBoomTexture.loadFromFile("Sprites/BombLose.png");
 
         for (int i = 1; i <= 6; ++i) {
             numberTextures.push_back(Texture());
-            numberTextures.back().loadFromFile("/Users/zakerden1234/Desktop/Minesweeper/Sprites/" + to_string(i) + ".png");
+            numberTextures.back().loadFromFile("Sprites/" + to_string(i) + ".png");
         }
 
-        restartButtonTexture.loadFromFile("/Users/zakerden1234/Desktop/Minesweeper/Sprites/Game.png");
+        restartButtonTexture.loadFromFile("Sprites/Game.png");
         restartButton.setTexture(restartButtonTexture);
         restartButton.setScale(60 / restartButton.getLocalBounds().width, 60 / restartButton.getLocalBounds().height);
-        restartButton.setPosition(window.getSize().x / 2.0f - restartButton.getLocalBounds().width / 0.75f,  20.0f);
+        restartButton.setPosition(window.getSize().x / 2.0f - restartButton.getLocalBounds().width / 0.75f, 20.0f);
 
         loadDigitTextures();
         loadRevealModeTextures();
+        loadTimerDigitTextures();
     }
 
     void handleButtonClick() {
@@ -388,20 +428,24 @@ public:
 
             if (restartButton.getGlobalBounds().contains(mousePosition.x, mousePosition.y)) {
                 game.StartGame();
+                timerStarted = false;
+                elapsedTime = 0.0f;
             }
         }
     }
 
+
     void updateButtonTexture() {
         if (game.isGameOver()) {
             if (game.getNormalGrid().getColumns() > 0 && game.win == false) {
-                restartButtonTexture.loadFromFile("/Users/zakerden1234/Desktop/Minesweeper/Sprites/Lose.png");
+                restartButtonTexture.loadFromFile("Sprites/Lose.png");
             }
-            else if(game.win == true){
-                restartButtonTexture.loadFromFile("/Users/zakerden1234/Desktop/Minesweeper/Sprites/Win.png");
+            else if (game.win == true) {
+                restartButtonTexture.loadFromFile("Sprites/Win.png");
             }
-        } else {
-            restartButtonTexture.loadFromFile("/Users/zakerden1234/Desktop/Minesweeper/Sprites/Game.png");
+        }
+        else {
+            restartButtonTexture.loadFromFile("Sprites/Game.png");
         }
 
         restartButton.setTexture(restartButtonTexture);
@@ -425,20 +469,25 @@ public:
                     if (cells[i][j].getIsMine()) {
                         if (cells[i][j].getIsExploded()) {
                             cellSprite.setTexture(bombBoomTexture);
-                        } else {
+                        }
+                        else {
                             cellSprite.setTexture(bombTexture);
                         }
-                    } else {
+                    }
+                    else {
                         int adjacentBombs = grid.countAdjacentBombs(i, j);
                         if (adjacentBombs == 0) {
                             cellSprite.setTexture(emptyTexture);
-                        } else {
+                        }
+                        else {
                             cellSprite.setTexture(numberTextures[adjacentBombs - 1]);
                         }
                     }
-                } else if (cells[i][j].getMarked()) {
+                }
+                else if (cells[i][j].getMarked()) {
                     cellSprite.setTexture(markedTexture);
-                } else {
+                }
+                else {
                     cellSprite.setTexture(unmarkedTexture);
                 }
 
@@ -451,6 +500,8 @@ public:
 
         drawNumber(game.getNormalGrid().mineCount - game.getNormalGrid().countMarkedCells(), 10.0f, 20.0f, 2.5f);
         drawRevealMode();
+
+        drawTimerDigits(373.0f, 20.0f, 2.5f);
 
         window.draw(restartButton);
         window.display();
@@ -469,20 +520,35 @@ public:
                     int x = mousePosition.x / 60;
                     int y = (mousePosition.y - 95) / 60;
 
+                    if (!timerStarted) {
+                        timerStarted = true;
+                        gameClock.restart();
+                    }
+
                     if (game.isRevealMode()) {
                         game.Click(x, y);
-                    } else {
+                    }
+                    else {
                         game.markCell(x, y);
                     }
                     handleButtonClick();
                 }
-            } else if (event.type == Event::KeyPressed && !game.isGameOver()) {
+            }
+            else if (event.type == Event::KeyPressed && !game.isGameOver()) {
                 if (event.key.code == Keyboard::F) {
                     game.toggleRevealMode();
                 }
             }
         }
+        if (timerStarted) {
+            elapsedTime = gameClock.getElapsedTime().asSeconds();
+        }
+
+        if (timerStarted && !game.isGameOver()) {
+            elapsedTime = gameClock.getElapsedTime().asSeconds();
+        }
     }
+
     RenderWindow window;
 };
 
